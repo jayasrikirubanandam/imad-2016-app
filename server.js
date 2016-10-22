a64d83b0227efa9be0031d683d7c55033d69292f
jayasrikirubanandam <kirubajayasri@gmail.com>
2016-10-22 20:01:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/jay2', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'jay2.html'));
 });
 
