e821bce320b2572009ec11c7a8ec2e0f1fcf4346
jayasrikirubanandam <kirubajayasri@gmail.com>
2016-11-11 19:03:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 console.log('Loaded!');
-var img = document.getelementbyid('madi');
+var img = document.getElementbyid('madi');
 img.onclick=function(){
     img.style.marginright='100px';
 };
-var showtext = document.getelementbyid('text');
+var showtext = document.getElementbyid('text');
 text.onclick=function(){
     text.style.marginright='100px';
 };
