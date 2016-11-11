bab768eecacad04f4e2a87ef663b50a811cc5f72
jayasrikirubanandam <kirubajayasri@gmail.com>
2016-11-11 18:14:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,17 @@ var img=document.getelementbyid('madi');
 img.onclick=function(){
     img.style.marginleft='100px';
 };
+var showText = function (target, message, index, interval) {   
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
+}
+
+$(function () {
+
+  showText("#msg", "hi", text, 500);   
+
+});
 
 
