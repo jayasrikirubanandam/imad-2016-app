74f2c2c933f75a6d26a3eca364668bb40ebc7060
jayasrikirubanandam <kirubajayasri@gmail.com>
2016-11-12 13:51:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,7 @@
 console.log('Loaded!');
-var img = document.getElementById('madi');
-img.onclick = function (){
+var showtext= document.getElementById('text');
+showtext.onclick = function (){
     img.style.marginLeft = '700px';
 };
-var showtext = document.getElementById('text');
-text.onclick=function (){
-    text.style.marginRight = '400px';
-};
+
 
